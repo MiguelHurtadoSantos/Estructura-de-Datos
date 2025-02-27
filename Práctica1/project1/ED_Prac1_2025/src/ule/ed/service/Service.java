26a09bf290513cf546905b4d677584564f869dd4
MiguelHurtadoSantos <mhurts00@estudiantes.unileon.es>
2025-02-27 13:49:23 +0100

    Modificaciones de empty y más métodos

---
+++ Práctica1/project1/ED_Prac1_2025/src/ule/ed/service/Service.java
@@ -0,0 +1,54 @@
+package ule.ed.service;
+
+import java.util.ArrayList;
+
+public class Service {
+	 private ArrayList<Dish> order;
+	 private int nChildren;
+	 private int nPeople;
+	 
+	 public Service(int nPeople, int nChildren) {
+		 // TODO
+		 this.nPeople = nPeople;
+		 this.nChildren = nChildren;
+		 }
+	 
+	 
+	 /**
+	  * Metodo que devuelve el numero de ninyos en una mesa
+	  * @return nChildren
+	  */
+	 public int getNChildren(){
+		 return this.nChildren;
+	 }
+	 
+	 /**
+	  * Metodo que devuelve el numero de personas de una mesa
+	  * @return nPeople
+	  */
+	 
+	 public int getNPeople() {
+		 return this.nPeople;
+	 }
+	 
+	 public ArrayList<Dish> getOrder(){
+		 return this.order;
+	 }
+	 
+	 
+	 //Calcula el total de este servicio, tiene que recorrer el array de platos calculando el total de cada plato, teniendo en cuenta que hay una cantidad de esos platos
+	 // total de un plato = count * price (número de platos * cantidad de platos)
+	 
+	 public double getTotalService() {
+		double total = 0;
+		getOrder();
+		return total;
+	 }
+	 
+	 @Override
+		public String toString() {
+			return "{Servicio:" + nPeople +"personas," +nChildren + "niños, total= "+ getTotalService() + "}";
+		}
+
+	}
+
